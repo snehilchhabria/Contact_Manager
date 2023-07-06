99a53f3532393e9c87a279f83773ebf6f43b0cf7
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-07-06 17:20:56 +0530

    userLoign

--- mycontacts-backend/controllers/userController.js
+++ mycontacts-backend/controllers/userController.js
@@ -1,6 +1,7 @@
 
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
 const User = require("../models/userModel");
 
 //@desc Register the user
@@ -36,7 +37,7 @@ const registerUser = asyncHandler (async (req,res) => {
     else{
         res.status(400)
         throw new Error("User data is not valid");
-    }
+    }s
     res.json("xxxxx");
 });
 
@@ -44,9 +45,31 @@ const registerUser = asyncHandler (async (req,res) => {
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler (async (req,res) => {
-    const{ username, password} = req.body;
-    if(!username || !password)
-    res.json({message :"Login the user"});
+    const{ email, password} = req.body;
+    if(!email || !password){
+        res.status(400);
+        throw new Error("User already registered")
+    }
+    const user = await User.findOne({email});
+    //compare password with hashedpasword
+    if( user && (await bcrypt.compare(password, user.password))){
+        const accessToken = jwt.sign({
+           user: {
+            username : user.username,
+            email:user.email,
+            id: user.id
+           },
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn : "1m"}
+
+        );
+        res.status(200).json({ accessToken });
+    }
+    else{
+        res.status(401);
+        throw new Error("email or password is not valid");
+    }
 });
 
 //@desc Current user
